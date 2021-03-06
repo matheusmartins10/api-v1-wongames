b6e01e7bbd096359b164f4c813beaae35f393303
Matheus Martins <mathews_2010@outlook.com>
2021-03-06 20:00:26 -0300

    api v1

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3a099a35917784308397a6b7b80755f2'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2e6b880c-8b03-4daa-b5ee-aab5476b5253'
+};
